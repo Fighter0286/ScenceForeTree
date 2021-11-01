71eea1d8ef19e8061a6d567b5d5af925f1be14c6
ZhangXuHui <zhang13623540286@136.com>
2021-11-01 14:27:47 +0800

    foretree
    四叉树

---
+++ Scripts/Tree.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tree : INode
+{
+    public Bounds bound { get; set; }
+    private Node root;
+    public int maxDepth { get; }
+    public int maxChildCount { get; }
+
+    public Tree(Bounds bound)
+    {
+        this.bound = bound;
+        this.maxDepth = 5;  //最大层级
+        this.maxChildCount = 4;  //最大孩子数量
+        root = new Node(bound, 0, this);  //结点
+    }
+
+    public void InsertObj(ObjData obj)
+    {
+        root.InsertObj(obj);
+    }
+
+    public void TriggerMove(Camera camera)
+    {
+        root.TriggerMove(camera);
+    }
+
+    public void DrawBound()
+    {
+        root.DrawBound();
+    }
+}
